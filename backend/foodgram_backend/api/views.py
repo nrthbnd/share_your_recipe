146613bab176a3328e9a01b96365eede92be44d5
nrthbnd <asoloshenkova@gmail.com>
2023-06-25 23:39:59 +0300

    Исправлены рецепты

--- backend/foodgram_backend/api/views.py
+++ backend/foodgram_backend/api/views.py
@@ -1,3 +1,125 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import get_object_or_404
+from rest_framework import exceptions, viewsets, status
+from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
+from rest_framework.response import Response
 
-# Create your views here.
+from recipes.models import Favorites, Ingredients, Recipes, ShoppingList, Tags
+from .filters import IngredientsFilter
+from .pagination import RecipesPagination
+from .serializers import (RecipesWriteSerializer, RecipesReadSerializer,
+                          RecipesMajorSerializer, TagsSerializer,
+                          IngredientsSerializer,)
+
+
+class RecipesViewSet(viewsets.ModelViewSet):
+    """Список рецептов."""
+    queryset = Recipes.objects.all()
+    # serializer_class = RecipesReadSerializer
+    pagination_class = RecipesPagination
+    # permission_classes = (,)
+    # filter_backends = (DjangoFilterBackend, SearchFilter)
+    # filterset_class = IngredientsFilter
+
+    def get_serializer_class(self):
+        """Выбор сериализатора для рецептов в зависимости от метода."""
+        if self.action in SAFE_METHODS:
+        # if self.request.method == 'GET':
+            return RecipesReadSerializer
+        return RecipesWriteSerializer
+
+    def perform_create(self, serializer):
+        """Сохранение рецепта с автором."""
+        serializer.save(author=self.request.user)
+
+    @action(detail=True, methods=['post', 'get'],
+            permission_classes=(IsAuthenticated,))
+    def favorite(self, request, pk=None):
+        """Проверяет, добавлен ли рецепт в избранное
+        и добавляет/удаляет его."""
+        user = self.request.user
+        recipe = get_object_or_404(Recipes, pk=pk)
+
+        if self.request.method == 'POST':
+            if Favorites.objects.filter(recipe=recipe, user=user).exists():
+                raise exceptions.ValidationError(
+                    'Рецепт уже добавлен в избранное!'
+                )
+            Favorites.objects.create(recipe=recipe, user=user)
+            serializer = RecipesMajorSerializer(
+                recipe,
+                context={'request': request}
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if self.request.method == 'DELETE':
+            if not Favorites.objects.filter(recipe=recipe, user=user).exists():
+                raise exceptions.ValidationError(
+                    'Рецепта нет в избранном!'
+                )
+            favorites = get_object_or_404(Favorites, recipe=recipe, user=user)
+            favorites.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    @action(detail=True, methods=['post', 'get'],
+            permission_classes=(IsAuthenticated,))
+    def shopping_cart(self, request, pk=None):
+        """Проверяет, добавлен ли рецепт в список покупок
+        и добавляет/удаляет его."""
+        user = self.request.user
+        recipe = get_object_or_404(Recipes, pk=pk)
+
+        if self.request.method == 'POST':
+            if ShoppingList.objects.filter(recipe=recipe, user=user).exists():
+                raise exceptions.ValidationError(
+                    'Рецепт уже добавлен в избранное!'
+                )
+            ShoppingList.objects.create(recipe=recipe, user=user)
+            serializer = RecipesMajorSerializer(
+                recipe,
+                context={'request': request}
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if self.request.method == 'DELETE':
+            if not ShoppingList.objects.filter(
+                recipe=recipe,
+                user=user
+            ).exists():
+
+                raise exceptions.ValidationError(
+                    'Рецепта нет в списке покупок!'
+                )
+            in_shopping_cart = get_object_or_404(
+                ShoppingList, recipe=recipe, user=user,
+            )
+            in_shopping_cart.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+class IngredientsViewSet(viewsets.ModelViewSet):
+    """Список ингредиентов."""
+    queryset = Ingredients.objects.all()
+    serializer_class = IngredientsSerializer
+    # permission_classes = (,)
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filterset_class = IngredientsFilter
+    search_fields = ('name',)
+
+
+class TagsViewSet(viewsets.ModelViewSet):
+    """Список тегов длфя рецептов."""
+    queryset = Tags.objects.all()
+    serializer_class = TagsSerializer
+    # permission_classes = (,)
+
+
+class RecipesInCartViewSet(viewsets.ModelViewSet):
+    queryset = Recipes.objects.all()
+    serializer_class = RecipesMajorSerializer
