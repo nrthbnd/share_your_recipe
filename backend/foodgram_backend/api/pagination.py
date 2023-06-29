21830ee293957373719c449eb4dcb30c6b80e151
nrthbnd <asoloshenkova@gmail.com>
2023-06-29 12:21:02 +0300

    Перенести магическое число в настройки

--- backend/foodgram_backend/api/pagination.py
+++ backend/foodgram_backend/api/pagination.py
@@ -1,10 +1,11 @@
+from django.conf import settings
 from rest_framework.pagination import PageNumberPagination
 
 
 class PageLimitPagination(PageNumberPagination):
     """Настраивает пагинацию в соответствии с
     /?page=<integer>&limit=<integer>"""
-    page_size = 6
+    page_size = settings.REST_FRAMEWORK['PAGE_SIZE']
     page_query_param = 'page'
     page_size_query_param = 'limit'
     page_query_description = 'Номер страницы.'
