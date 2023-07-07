2fffadc29ad486f46eb60d3794987ec55182ea26
nrthbnd <asoloshenkova@gmail.com>
2023-07-07 16:20:31 +0300

    Add if sett.DEBUG

--- backend/foodgram_backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/foodgram_backend/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -6,3 +8,9 @@ urlpatterns = [
     path('api/', include('users.urls')),
     path('api/', include('api.urls', namespace='api')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
